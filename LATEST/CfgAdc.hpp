fa8470f23894464e7d227d75c74f90ea179f2287
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-05 09:10:25 +0000

    Update: Generated configurations for AUTOSAR stack

--- LATEST/CfgAdc.hpp
+++ LATEST/CfgAdc.hpp
@@ -24,6 +24,45 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
+typedef enum{
+      eModeConversion_OneShot = 0
+   ,  eModeConversion_Continuous
+}Adc_TypeModeConversion;
+
+typedef enum{
+      eSourceTrigger_EventSoftware = 0
+   ,  eSourceTrigger_EventHardware
+}Adc_TypeSourceTrigger;
+
+typedef struct{
+}Adc_TypeChannel;
+
+typedef enum{
+      eStateGroup_Suspend = 0
+   ,  eStateGroup_Resume
+}Adc_TypeStateGroup;
+
+typedef struct{
+   Adc_TypeChannel*      ptrListChannel;
+   Adc_TypeSourceTrigger SourceTrigger;
+   uint8                 LevelPriority;
+   Adc_TypeStateGroup    StateGroup;
+}Adc_TypeChannelGroup;
+
+typedef struct{
+}Adc_TypeResultsGroupConversion;
+
+typedef enum{
+      eStatusGroup_Unknown = 0
+   ,  eStatusGroup_StreamCompleted
+}Adc_TypeStatusGroup;
+
+typedef struct{
+   Adc_TypeResultsGroupConversion* ptrListResultsGroupConversion;
+   uint8                           NumberResultsValid; // = 1, for eModeConversion_OneShot 
+   Adc_TypeStatusGroup             StatusGroup;
+}Adc_TypeBufferResults;
+
 class CfgAdc_Type:
       public CfgModule_TypeAbstract
 {
