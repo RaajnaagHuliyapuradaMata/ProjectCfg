218b3230c64becaf0c8f4b5e8a1b83a71b1acead
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-16 18:27:40 +0000

    Update: Generated configurations for AUTOSAR stack

--- LATEST/CfgPduR.hpp
+++ LATEST/CfgPduR.hpp
@@ -31,27 +31,27 @@ typedef enum{
 }TypeState;
 
 typedef uint16 TypeId;
+typedef TypeId TypeIdRoutingPathGroup;
 
 typedef struct{
-   //TBD: bit fields?
-   bool bUpperModule;
-   bool bLowerModule;
-   bool bCommunicationInterface;
-   bool bTransportProtocol;
-   bool bTransmit;
-   bool bTransmitTp;
-   bool bTxConfirmation;
-   bool bTxConfirmationTp;
-   bool bRxIndication;
-   bool bRxIndicationTp;
-   bool bCancelTransmit;
-   bool bCancelReceive;
-   bool bCopyTxData;
-   bool bCopyRxData;
-   bool bTriggerTransmit;
-   bool bReTransmition;
-   bool bStartOfReception;
-   bool bUseTag;
+   uint32 bUpperModule            :1;
+   uint32 bLowerModule            :1;
+   uint32 bCommunicationInterface :1;
+   uint32 bTransportProtocol      :1;
+   uint32 bTransmit               :1;
+   uint32 bTransmitTp             :1;
+   uint32 bTxConfirmation         :1;
+   uint32 bTxConfirmationTp       :1;
+   uint32 bRxIndication           :1;
+   uint32 bRxIndicationTp         :1;
+   uint32 bCancelTransmit         :1;
+   uint32 bCancelReceive          :1;
+   uint32 bCopyTxData             :1;
+   uint32 bCopyRxData             :1;
+   uint32 bTriggerTransmit        :1;
+   uint32 bReTransmition          :1;
+   uint32 bStartOfReception       :1;
+   uint32 bUseTag                 :1;
 }TypeClient;
 
 typedef struct{
@@ -60,8 +60,9 @@ typedef struct{
 }TypeRoutingPath;
 
 typedef struct{
-   uint16          u16CntMaxRoutingPath;
-   TypeRoutingPath astGroup[];
+   TypeIdRoutingPathGroup Id;
+   uint16                 u16CntMaxRoutingPath;
+   TypeRoutingPath        astGroup[];
 }TypeRoutingPathGroup;
 
 class CfgPduR_Type:
