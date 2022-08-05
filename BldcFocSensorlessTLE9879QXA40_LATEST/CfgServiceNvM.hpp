fa7b05415d41244c01083f42f17adae870008ddb
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-05 14:34:49 +0200

    Update: Configurations based generated code for AUTOSAR stack

---
+++ BldcFocSensorlessTLE9879QXA40_LATEST/CfgServiceNvM.hpp
@@ -0,0 +1,73 @@
+#pragma once
+/******************************************************************************/
+/* File   : CfgServiceNvM.hpp                                                        */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CfgModule.hpp"
+#include "CompilerCfg_ServiceNvM.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CFGSERVICENVM_AR_RELEASE_VERSION_MAJOR                                        4
+#define CFGSERVICENVM_AR_RELEASE_VERSION_MINOR                                        3
+#define ServiceNvM_DevErrorDetect                                        DevErrorDetect
+#define ServiceNvM_InitCheck                                                     STD_ON
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(CFGSERVICENVM_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible CFGSERVICENVM_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(CFGSERVICENVM_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible CFGSERVICENVM_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+typedef enum{
+      eResultRequest_Ok = 0
+   ,  eResultRequest_NotOk
+   ,  eResultRequest_Pending
+   ,  eResultRequest_FailedIntegrity
+   ,  eResultRequest_BlockSkipped
+   ,  eResultRequest_InvalidatedNv
+   ,  eResultRequest_McalCancled
+   ,  eResultRequest_FailedRedundancy
+   ,  eResultRequest_RestoredMcalFromRom
+}TypeResultRequest;
+
+typedef uint16 TypeIdBlock;
+
+class CfgServiceNvM_Type:
+      public CfgModule_TypeAbstract
+{
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
---
+++ BldcFocSensorlessTLE9879QXA40_LATEST/infCfg_Imp.hpp
@@ -0,0 +1,103 @@
+#pragma once
+/******************************************************************************/
+/* File   : infCfgGen_Imp.hpp                                                 */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+
+#include "ApplSwcFoc.hpp"
+#include "ProjectARA.hpp"
+#include "EcuabCanIf.hpp"
+#include "EcuabCanTp.hpp"
+#include "EcuabCryIf.hpp"
+#include "EcuabEa.hpp"
+#include "EcuabEthIf.hpp"
+#include "EcuabFee.hpp"
+#include "EcuabFrIf.hpp"
+#include "EcuabFrTp.hpp"
+#include "EcuabJ1939Tp.hpp"
+#include "EcuabLinIf.hpp"
+#include "EcuabLinTp.hpp"
+#include "EcuabMemIf.hpp"
+#include "EcuabWdgIf.hpp"
+#include "McalAdc.hpp"
+#include "McalCan.hpp"
+#include "McalCry.hpp"
+#include "McalDio.hpp"
+#include "McalEep.hpp"
+#include "McalEth.hpp"
+#include "McalFls.hpp"
+#include "McalFr.hpp"
+#include "McalGpt.hpp"
+#include "McalIcu.hpp"
+#include "McalLin.hpp"
+#include "McalMcu.hpp"
+#include "McalOcu.hpp"
+#include "McalPort.hpp"
+#include "McalPwm.hpp"
+#include "McalSpi.hpp"
+#include "McalWdg.hpp"
+#include "ServiceBswM.hpp"
+#include "ServiceCanNm.hpp"
+#include "ServiceCanSm.hpp"
+#include "ServiceCom.hpp"
+#include "ServiceComM.hpp"
+#include "ServiceCsm.hpp"
+#include "ServiceDcm.hpp"
+#include "ServiceDem.hpp"
+#include "ServiceDet.hpp"
+#include "ServiceDlt.hpp"
+#include "ServiceEcuM.hpp"
+#include "ServiceFiM.hpp"
+#include "ServiceFrNm.hpp"
+#include "ServiceIpduM.hpp"
+#include "ServiceNm.hpp"
+#include "ServiceNvM.hpp"
+#include "ServiceOs.hpp"
+#include "ServicePduR.hpp"
+#include "ServiceSchM.hpp"
+#include "ServiceSecOC.hpp"
+#include "ServiceSokFm.hpp"
+#include "ServiceStartUp.hpp"
+#include "ServiceStbM.hpp"
+#include "ServiceSwcEcuM.hpp"
+#include "ServiceSwcOs.hpp"
+#include "ServiceVkms.hpp"
+#include "ServiceWdgM.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
